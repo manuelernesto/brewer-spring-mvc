4cb7b42757f207c09893b99eefb7d3a342d4eea9
manuelernesto <manuelernesto@outlook.com>
2020-09-03 13:19:46 +0100

    add dropdown sabor to CadastroCerveja.html

--- src/main/java/io/github/manuelernesto/controller/CervejaController.java
+++ src/main/java/io/github/manuelernesto/controller/CervejaController.java
@@ -1,6 +1,7 @@
 package io.github.manuelernesto.controller;
 
 import io.github.manuelernesto.Model.Cerveja;
+import io.github.manuelernesto.Model.Sabor;
 import io.github.manuelernesto.repository.Cervejas;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -24,20 +26,21 @@ public class CervejaController {
     }
 
     @RequestMapping("/cerveja/novo")
-    public String novo(Cerveja cerveja) {
-        cervejas.findBySku("JJAJ");
-        return "cerveja/CadastroCerveja";
+    public ModelAndView novo(Cerveja cerveja) {
+        ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+        mv.addObject("sabores", Sabor.values());
+        return mv;
     }
 
     @RequestMapping(value = "/cerveja/novo", method = RequestMethod.POST)
-    public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+    public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
         if (result.hasErrors())
             return novo(cerveja);
 
 
         attributes.addFlashAttribute("mensagem", cerveja.getSku() + " Cerveja Cadastrada com sucesso!");
 
-        return "redirect:/cerveja/novo";
+        return new ModelAndView("redirect:/cerveja/novo");
     }
 
 }
