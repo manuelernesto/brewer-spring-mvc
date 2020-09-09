7a1a91e5657042a1e6ec6d07eea386f41c03e20a
manuelernesto <manuelernesto@outlook.com>
2020-09-09 10:46:19 +0100

    upload phone with ajax

--- src/main/java/io/github/manuelernesto/config/init/AppInitializer.java
+++ src/main/java/io/github/manuelernesto/config/init/AppInitializer.java
@@ -7,6 +7,8 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -32,6 +34,11 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
         characterEncodingFilter.setEncoding("UTF-8");
         characterEncodingFilter.setForceEncoding(true);
         return new Filter[]{characterEncodingFilter};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(new MultipartConfigElement(""));
     }
 }
 
