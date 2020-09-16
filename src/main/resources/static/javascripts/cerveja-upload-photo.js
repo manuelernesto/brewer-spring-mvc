dbe734e3de045aa2a1663c6acef2559350fbb399
manuelernesto <manuelernesto@outlook.com>
2020-09-16 22:15:00 +0100

    refactoring js for save photo

--- src/main/resources/static/javascripts/cerveja-upload-photo.js
+++ src/main/resources/static/javascripts/cerveja-upload-photo.js
@@ -1,99 +1,66 @@
-// var Brewer = Brewer || {};
-//
-// Brewer.PhotoUpload = (function () {
-//     function PhotoUpload() {
-//         this.inputPhotoName = $('input[name=foto]');
-//         this.inputContentType = $('input[name=contentType]');
-//
-//         this.htmlFotoCervejaTemplate = $("#foto-cerveja").html();
-//         this.template = Handlebars.compile(this.htmlFotoCervejaTemplate);
-//
-//         this.containerPhotoCerveja = $('.js-container-photo-cerveja');
-//
-//         this.uploadDrop = $('#upload-drop');
-//     }
-//
-//     PhotoUpload.prototype.init = function () {
-//         let settings = {
-//             type: 'json',
-//             filelimit: 1,
-//             allow: '*.(jpg|jpeg|png)',
-//             action: this.containerPhotoCerveja.data('url-photo'),
-//             complete: onUploadComplete.bind(this)
-//         }
-//
-//         UIkit.uploadSelect($('#upload-select'), settings);
-//         UIkit.uploadDrop($(this.uploadDrop), settings);
-//
-//         if (this.inputPhotoName.val()) {
-//             onUploadComplete.call(this, {nome: this.inputPhotoName.val(), contentType: this.inputContentType.val()});
-//         }
-//     }
-//
-//     function onUploadComplete(response) {
-//         this.inputPhotoName.val(response.nome);
-//         this.inputContentType.val(response.contentType);
-//
-//         this.uploadDrop.addClass('hidden');
-//         let htmlFotoCerveja = this.template({photoName: response.nome})
-//
-//         this.containerPhotoCerveja.append(htmlFotoCerveja);
-//
-//         $('.js-remove-photo').on('click', onRemovePhoto.bind(this));
-//     }
-//
-//     function onRemovePhoto() {
-//         $('.js-photo-cerveja').remove();
-//         this.uploadDrop.removeClass('hidden');
-//         this.inputPhotoName.val('');
-//         this.inputContentType.val('');
-//     }
-//
-//     return PhotoUpload;
-// })();
-//
-//
-// $(function () {
-//     let photoUpload = new Brewer.PhotoUpload();
-//     photoUpload.init();
-// });
+var Brewer = Brewer || {};
 
-$(function () {
+Brewer.UploadFoto = (function () {
 
-    let settings = {
-        type: 'json',
-        filelimit: 1,
-        allow: '*.(jpg|jpeg|png)',
-        action: '/brewer/fotos',
-        complete: function (response) {
-            let inputNomeFoto = $('input[name=foto]');
-            let inputContentType = $('input[name=contentType]');
-            let htmlFotoCervejaTemplate = $("#foto-cerveja").html();
-            let template = Handlebars.compile(htmlFotoCervejaTemplate);
-            let htmlFotoCerveja = template({nomeFoto: response.nome});
-            let containerPhotoCerveja = $('.js-container-foto-cerveja');
-            let uploaDrop = $('#upload-drop');
+    function UploadFoto() {
+        this.inputNomeFoto = $('input[name=foto]');
+        this.inputContentType = $('input[name=contentType]');
 
-            inputNomeFoto.val(response.nome);
-            inputContentType.val(response.contentType);
+        this.htmlFotoCervejaTemplate = $("#foto-cerveja").html();
+        this.template = Handlebars.compile(this.htmlFotoCervejaTemplate);
 
-            uploaDrop.addClass('hidden');
-
-            containerPhotoCerveja.append(htmlFotoCerveja);
-
-            $('.js-remove-foto').on('click', function () {
-                $('.js-foto-cerveja').remove();
-                uploaDrop.removeClass('hidden');
-                inputNomeFoto.val('');
-                inputContentType.val('');
-            });
-        }
+        this.containerPhotoCerveja = $('.js-container-foto-cerveja');
+        this.uploaDrop = $('#upload-drop');
     }
 
-    UIkit.uploadSelect($('#upload-select'), settings);
-    UIkit.uploadDrop($(this.uploadDrop), settings);
+    UploadFoto.prototype.init = function () {
+        let settings = {
+            type: 'json',
+            filelimit: 1,
+            allow: '*.(jpg|jpeg|png)',
+            action: this.containerPhotoCerveja.data('url-fotos'),
+            complete: onUploadComplete.bind(this)
+        }
 
+        UIkit.uploadSelect($('#upload-select'), settings);
+        UIkit.uploadDrop($(this.uploadDrop), settings);
+
+        // if (this.inputNomeFoto.val()) {
+        //     onUploadComplete.call(this, {nome: this.inputNomeFoto.val(), contentType: this.inputContentType.val()});
+        // }
+    }
+
+    function onUploadComplete(response) {
+        this.inputNomeFoto.val(response.nome);
+        this.inputContentType.val(response.contentType);
+
+        this.uploadDrop.addClass('hidden');
+
+        let htmlFotoCerveja = this.template({nomeFoto: this.response.nome});
+
+        this.containerPhotoCerveja.append(htmlFotoCerveja);
+
+        $('.js-remove-foto').on('click', onRemovePhoto.bind(this));
+    }
+
+
+    function onRemovePhoto() {
+        $('.js-foto-cerveja').remove();
+        this.uploaDrop.removeClass('hidden');
+        this.inputNomeFoto.val('');
+        this.inputContentType.val('');
+    }
+
+    return UploadFoto();
+})();
+
+
+$(function () {
+    let uploadFoto = new Brewer.UploadFoto();
+    uploadFoto.init();
 });
+
+
 
 
 
