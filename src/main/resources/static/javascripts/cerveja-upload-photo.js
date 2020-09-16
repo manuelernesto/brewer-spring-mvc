6c2ab9731c7e42a480d60a3ddfa1485c336fd6cc
manuelernesto <manuelernesto@outlook.com>
2020-09-16 22:36:29 +0100

    refactoring js for save photo

--- src/main/resources/static/javascripts/cerveja-upload-photo.js
+++ src/main/resources/static/javascripts/cerveja-upload-photo.js
@@ -25,9 +25,9 @@ Brewer.UploadFoto = (function () {
         UIkit.uploadSelect($('#upload-select'), settings);
         UIkit.uploadDrop($(this.uploadDrop), settings);
 
-        // if (this.inputNomeFoto.val()) {
-        //     onUploadComplete.call(this, {nome: this.inputNomeFoto.val(), contentType: this.inputContentType.val()});
-        // }
+        if (this.inputNomeFoto.val()) {
+            onUploadComplete.call(this, {nome: this.inputNomeFoto.val(), contentType: this.inputContentType.val()});
+        }
     }
 
     function onUploadComplete(response) {
