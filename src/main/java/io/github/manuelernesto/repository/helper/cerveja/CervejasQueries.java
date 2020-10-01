59e755c45394ef30a4554198b588ef1e37728de3
manuelernesto <manuelernesto@outlook.com>
2020-10-01 15:50:53 +0100

    prototype the order by filter

--- src/main/java/io/github/manuelernesto/repository/helper/cerveja/CervejasQueries.java
+++ src/main/java/io/github/manuelernesto/repository/helper/cerveja/CervejasQueries.java
@@ -8,5 +8,5 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface CervejasQueries {
-    public Page<Cerveja> filtrar(CervejaFilter filter, Pageable pageable);
+    Page<Cerveja> filtrar(CervejaFilter filter, Pageable pageable);
 }
