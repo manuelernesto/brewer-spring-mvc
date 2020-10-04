0e17867b45e8a0a03cc46f36d1a10b72a6274abe
manuelernesto <manuelernesto@outlook.com>
2020-10-04 18:52:27 +0100

    sorting filter

--- src/main/java/io/github/manuelernesto/thymeleaf/BrewerDialect.java
+++ src/main/java/io/github/manuelernesto/thymeleaf/BrewerDialect.java
@@ -3,6 +3,7 @@ package io.github.manuelernesto.thymeleaf;
 
 import io.github.manuelernesto.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
 import io.github.manuelernesto.thymeleaf.processor.MessageElementTagProcessor;
+import io.github.manuelernesto.thymeleaf.processor.OrderElementTagProcessor;
 import org.thymeleaf.dialect.AbstractProcessorDialect;
 import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
@@ -25,6 +26,7 @@ public class BrewerDialect extends AbstractProcessorDialect {
         final Set<IProcessor> processors = new HashSet<>();
         processors.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
         processors.add(new MessageElementTagProcessor(dialectPrefix));
+        processors.add(new OrderElementTagProcessor(dialectPrefix));
         return processors;
     }
 }
