f259c96310e98d0a36e0378493df73e139b79ee7
manuelernesto <manuelernesto@outlook.com>
2020-10-05 22:13:47 +0100

    staring implement Bean util for pagination

--- src/main/java/io/github/manuelernesto/repository/helper/cerveja/CervejasImpl.java
+++ src/main/java/io/github/manuelernesto/repository/helper/cerveja/CervejasImpl.java
@@ -6,28 +6,27 @@ import io.github.manuelernesto.repository.pagination.PaginacaoUtil;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.List;
 
 public class CervejasImpl implements CervejasQueries {
 
     @PersistenceContext
     private EntityManager manager;
 
-    @Autowired
-    private PaginacaoUtil paginacaoUtil;
+    private final PaginacaoUtil paginacaoUtil;
+
+    public CervejasImpl(PaginacaoUtil paginacaoUtil) {
+        this.paginacaoUtil = paginacaoUtil;
+    }
 
     @SuppressWarnings("unchecked")
     @Transactional(readOnly = true)
